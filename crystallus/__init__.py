b723615851e228f14ecb4a627df29c64bfedb672
tsumina <liu.chang.1865@gmail.com>
2020-07-03 06:53:26 +0900

    update dependencies; finish feature

--- crystallus/__init__.py
+++ crystallus/__init__.py
@@ -3,7 +3,11 @@ import mkl as _  # for side effect
 from .wyckoff_cfg_generator import WyckoffCfgGenerator
 from .crystal_generator import CrystalGenerator
 from .wyckoff_db import Wyckoff as WyckoffDB, SpaceGroup as SpaceGroupDB
+from .crystallus import lll_reduce, pbc_all_distances
 
-__all__ = ["CrystalGenerator", "WyckoffCfgGenerator", "WyckoffDB", "SpaceGroupDB"]
+__all__ = [
+    "CrystalGenerator", "WyckoffCfgGenerator", "WyckoffDB", "SpaceGroupDB", "pbc_all_distances",
+    "lll_reduce"
+]
 
-__version__ = "0.1.4"
+__version__ = "0.1.5-beta"
